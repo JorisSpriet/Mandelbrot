c42545b02ea0da60fb9d18dccd8a4789fb1bd21a
Joris Spriet <Joris.Spriet@brighteye.be>
2019-09-01 16:08:55 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace MandelBrotParallel
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.settingsBox = new System.Windows.Forms.GroupBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.steps = new System.Windows.Forms.TextBox();
+			this.toPower = new System.Windows.Forms.TextBox();
+			this.fromPower = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.settingsBox.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Location = new System.Drawing.Point(-1, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(882, 623);
+			this.panel1.TabIndex = 0;
+			// 
+			// settingsBox
+			// 
+			this.settingsBox.Controls.Add(this.button3);
+			this.settingsBox.Controls.Add(this.steps);
+			this.settingsBox.Controls.Add(this.toPower);
+			this.settingsBox.Controls.Add(this.fromPower);
+			this.settingsBox.Controls.Add(this.button2);
+			this.settingsBox.Controls.Add(this.button1);
+			this.settingsBox.Dock = System.Windows.Forms.DockStyle.Right;
+			this.settingsBox.Location = new System.Drawing.Point(887, 0);
+			this.settingsBox.Name = "settingsBox";
+			this.settingsBox.Size = new System.Drawing.Size(229, 624);
+			this.settingsBox.TabIndex = 1;
+			this.settingsBox.TabStop = false;
+			this.settingsBox.Text = "Settings";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(131, 97);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 6;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// steps
+			// 
+			this.steps.Location = new System.Drawing.Point(98, 19);
+			this.steps.Name = "steps";
+			this.steps.Size = new System.Drawing.Size(100, 20);
+			this.steps.TabIndex = 5;
+			this.steps.Text = "1000";
+			// 
+			// toPower
+			// 
+			this.toPower.Location = new System.Drawing.Point(54, 19);
+			this.toPower.Name = "toPower";
+			this.toPower.Size = new System.Drawing.Size(38, 20);
+			this.toPower.TabIndex = 4;
+			this.toPower.Text = "4";
+			// 
+			// fromPower
+			// 
+			this.fromPower.Location = new System.Drawing.Point(6, 19);
+			this.fromPower.Name = "fromPower";
+			this.fromPower.Size = new System.Drawing.Size(42, 20);
+			this.fromPower.TabIndex = 2;
+			this.fromPower.Text = "2";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(6, 126);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(6, 97);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1116, 624);
+			this.Controls.Add(this.settingsBox);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.settingsBox.ResumeLayout(false);
+			this.settingsBox.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.GroupBox settingsBox;
+		private System.Windows.Forms.TextBox fromPower;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox steps;
+		private System.Windows.Forms.TextBox toPower;
+		private System.Windows.Forms.Button button3;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Encoder = System.Drawing.Imaging.Encoder;
+
+namespace MandelBrotParallel
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var power = double.Parse(fromPower.Text);
+
+
+			Launch(power);
+		}
+
+		private void Launch(double p, int? n = null)
+		{
+
+
+			var w = Convert.ToDouble(panel1.Width);
+			var h = Convert.ToDouble(panel1.Height);
+			var rh = 3.0 / h;
+
+			Graphics g = null;
+			Bitmap bm = null;
+			var save = n.HasValue;
+			if (!save)
+				g = panel1.CreateGraphics();
+			else
+			{
+				bm = new Bitmap(1080, 1080, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+				w = h = 1080d;
+				rh = 3.0 / 1080d;
+				g = Graphics.FromImage(bm);
+			}
+			g.Clear(Color.White);
+
+			var hdc = g.GetHdc();
+
+			for (int x = 0; x < w; x++)
+			{
+				for (int y = 0; y < h; y++)
+				{
+
+					var a = 1.0 - (w - (double)x) * rh;
+
+					var b = 1.5 - ((double)y) * rh;
+
+					var z = new complex(a, b);
+
+					var r = Iterate(z, p);
+					if (r)
+						GDI.SetPixel(hdc, x, y, 0x00000000);
+				}
+			}
+			g.ReleaseHdc(hdc);
+
+
+			g.DrawString(p.ToString("0.000"), SystemFonts.DefaultFont, Brushes.Black, 50, 50);
+
+			if (save)
+			{
+				bm.Save($@"d:\brol\mb\mb{n.Value.ToString("d4")}.png", ImageFormat.Png);
+
+				Invoke(new MethodInvoker(delegate
+				{
+
+					var g2 = panel1.CreateGraphics();
+					g2.DrawImage(bm, new Point(0, 0));
+					g2.Dispose();
+					bm.Dispose();
+				}));
+
+
+			}
+			g.Dispose();
+
+		}
+
+
+		private bool Iterate(complex c, double p)
+		{
+			var z = new complex(.0d, .0d);
+
+			for (int i = 0; i < 50; i++)
+			{
+				var r = (polar)z;
+				r = r.Pow(p);
+				z = (complex)r + c;
+				if (z.Abs2() > 4.0d) return false;
+			}
+
+			return z.Abs2() < 4.0d;
+
+		}
+
+		public class GDI
+		{
+			[System.Runtime.InteropServices.DllImport("gdi32.dll")]
+			internal static extern bool SetPixel(IntPtr hdc, int X, int Y, uint crColor);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			panel1.Invalidate();
+		}
+
+		private async void button3_Click(object sender, EventArgs e)
+		{
+			var start = double.Parse(fromPower.Text);
+			var end = double.Parse(toPower.Text);
+			var n = (double)int.Parse(steps.Text);
+			var inc = (end - start) / n;
+			var p = start;
+			var t = 0;
+
+			button3.Enabled = false;
+
+			await Task.Run(() =>
+			{
+				do
+				{
+					Launch(p, t++);
+					p += inc;
+				} while (p <= end+.001d);
+			});
+
+			button3.Enabled = true;
+		}		
+	}
+
+	public class complex
+	{
+		public double x;
+		public double y;
+
+		public complex(double a, double b)
+		{
+			x = a;
+			y = b;
+		}
+
+		public static complex operator *(complex z1, complex z2)
+		{
+			var a = z1.x * z2.x - z1.y * z2.y;
+			var b = z1.x * z2.y + z1.y * z2.x;
+			return new complex(a, b);
+		}
+
+		public static complex operator +(complex z1, complex z2)
+		{
+			return new complex(z1.x + z2.x, z1.y + z2.y);
+		}
+
+		public double Abs2()
+		{
+			return x * x + y * y;
+		}
+	}
+
+	public class polar
+	{
+		public double r;
+		public double th;
+
+		public polar(double a, double b)
+		{
+			r = a;
+			th = b;
+		}
+
+		public static explicit operator polar(complex z)
+		{
+			var r = Math.Sqrt(z.Abs2());
+			double th;
+			if (z.y == 0d) th = Math.PI / 2.0d;
+			else th = Math.Atan2(z.y, z.x);
+			return new polar(r, th);
+		}
+
+		public static explicit operator complex(polar z)
+		{
+			var a = z.r * Math.Cos(z.th);
+			var b = z.r * Math.Sin(z.th);
+			return new complex(a, b);
+
+		}
+
+
+
+		public polar Pow(double z)
+		{
+			return new polar(Math.Pow(r, z), z * th);
+		}
+	}
+}
